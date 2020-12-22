0a4e808c412f053ca8264964de559f747fb1ec6a
maximeoger <maximeoger93@gmail.com>
2020-12-22 16:52:06 +0100

    fix(common): refs feat/login, fixed validation error message in login form validation constraints

--- packages/common/src/business/user/validation/login.ts
+++ packages/common/src/business/user/validation/login.ts
@@ -1,15 +1,13 @@
 export const loginValidationConstraints = {
   login: {
-    presence: true,
+    presence: { message: "Veuillez entrer votre addresse email." },
     email: true,
-    message: "Veuillez entrer votre addresse email.",
   },
   password: {
-    presence: true,
+    presence: { message: "Veuillez entrer votre mot de passe" },
     length: {
       minimum: 6,
       message: "Votre mot de passe doit contenir au moins 6 caractères"
-    },
-    message: "Veuillez entrer votre mot de passe",
+    }
   }
 } 
