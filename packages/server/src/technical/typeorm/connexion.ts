d14064e97a368b82a7b9a0723322ec6ec04c8991
maximeoger <maximeoger93@gmail.com>
2021-01-11 17:00:12 +0100

    fix: typeorm could not find the ormconfig file and returned a bug about import statement when running node from /dist/index.js + removed skip lib check

--- packages/server/src/technical/typeorm/connexion.ts
+++ packages/server/src/technical/typeorm/connexion.ts
@@ -1,16 +1,14 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-import { getConnectionOptions, createConnection as TypeOrmCreateConnexion, Connection } from 'typeorm';
+import { createConnection as TypeOrmCreateConnexion, Connection } from 'typeorm';
+import connectionOptions from './utils/connectionOptions';
 
 export async function createConnection(handleError: (error: Error) => unknown): Promise<Connection>{
-  return getConnectionOptions()
-    .then(connectionOptions => {
-      return TypeOrmCreateConnexion({ 
-        ...(connectionOptions as PostgresConnectionOptions),
-        // poolErrorHandler: handleError,
-        // extra: {
-        //   max: 1
-        // },
-      })
+    return TypeOrmCreateConnexion({ 
+      ...(connectionOptions as PostgresConnectionOptions),
+      poolErrorHandler: handleError,
+      extra: {
+        max: 1
+      },
     })
   .catch(error => {
     handleError(error);
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 yarn --cwd common link
 yarn --cwd server link common
-node --input-type=module dist/src/index.js 
+node --input-type=module --require dotenv/config dist/index.js 
