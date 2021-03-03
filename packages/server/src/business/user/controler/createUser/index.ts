071deaa06713b5a62c905558b369c9b615a5497a
maximeoger <maximeoger93@gmail.com>
2021-03-03 16:10:38 +0100

    feat(server/email-verification): switcher user hasing password fn from subscribers to user controller to prevent entities insertion conflicts

--- packages/server/src/business/user/controler/createUser/index.ts
+++ packages/server/src/business/user/controler/createUser/index.ts
@@ -2,11 +2,15 @@ import ControllerInterface from '../../../../technical/controller/controllerInte
 import { getUserRepository } from '../../../../business/user/repository/user';
 import { createUserEntity } from '../../entity/user.entity';
 import { IUserResponse } from 'common/dist/business/user';
+import { createEmailVerificationEntity } from '../../../email_verification/entity/email_verification.entity';
 import { apiExtractor } from '../../../../technical/user/apiExtractor';
 import { generateToken, TOKEN_MAX_AGE } from '../../../../technical/user/jwtHandler';
+import { getManager } from 'typeorm';
+import { hashPassword } from '../../../../technical/user/passwordHandler';
 
 const createUserController: ControllerInterface<IUserResponse> = async function UserGetController(req, res) {
   const userRepository = getUserRepository();
+
   let user = await createUserEntity(req.body);
 
   const userExists = await userRepository.findOne({
@@ -17,8 +21,15 @@ const createUserController: ControllerInterface<IUserResponse> = async function 
     throw new Error('user exist');
   }
 
-  user = await userRepository.save(user);
+  let emailVerification = await createEmailVerificationEntity(user);
 
+  user = await hashPassword(user);
+
+  await getManager().transaction(async entityManager => {
+    await entityManager.save(user);
+    await entityManager.save(emailVerification);
+  })
+  
   const responseData = await apiExtractor(user); 
   const access_token = await generateToken(responseData);
   res.cookie('access_token', access_token , { httpOnly: true, maxAge: TOKEN_MAX_AGE });
--- packages/server/src/business/user/subscribers/user.subscriber.ts
+++ packages/server/src/business/user/subscribers/user.subscriber.ts
@@ -1,16 +1,5 @@
-import { EventSubscriber, EntitySubscriberInterface, InsertEvent } from "typeorm";
+import { EventSubscriber, EntitySubscriberInterface } from "typeorm";
 import { UserEntity } from '../../user/entity/user.entity';
-import { hashPassword } from '../../../technical/user/passwordHandler';
-import { createEmailVerificationEntity } from '../../email_verification/entity/email_verification.entity';
-
 
 @EventSubscriber()
-export class UserSubscriber implements EntitySubscriberInterface {
-  async beforeInsert(event: InsertEvent<UserEntity>) {
-    return await hashPassword(event.entity);
-  }
-
-  async afterInsert(event: InsertEvent<UserEntity>){
-    return await createEmailVerificationEntity(event.entity);
-  }
-}
+export class UserSubscriber implements EntitySubscriberInterface<UserEntity> {}
