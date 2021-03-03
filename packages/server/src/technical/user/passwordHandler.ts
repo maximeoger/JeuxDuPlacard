257a0bb2194dd7c37733d5155f69a917017d9fd7
maximeoger <maximeoger93@gmail.com>
2021-03-03 16:08:32 +0100

    feat(server/email-verification): updated password handler to return User entity

--- packages/server/src/technical/user/passwordHandler.ts
+++ packages/server/src/technical/user/passwordHandler.ts
@@ -1,9 +1,10 @@
 import bcrypt from 'bcrypt';
 import { UserEntity } from '../../business/user/entity/user.entity';
 
-export const hashPassword = async (entity: UserEntity): Promise<void> => {
+export const hashPassword = async (entity: UserEntity): Promise<UserEntity> => {
   const hashedPassword = await bcrypt.hash(entity.password, 10);
   entity.password = hashedPassword;
+  return entity;
 }
 
 export const comparePasswords = async (password: string, hash: string): Promise<boolean> => {
