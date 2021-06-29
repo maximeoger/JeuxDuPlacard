0d76447c931085f5c24b916194b333b7d6e8551e
maximeoger <maximeoger93@gmail.com>
2021-06-29 14:15:04 +0200

    add heroku.yml

--- packages/server/ormconfig.js
+++ packages/server/ormconfig.js
@@ -3,6 +3,7 @@ const PORT = process.env.DB_PORT;
 const USER_NAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 const DATABASE = process.env.DB_NAME;
+const URL = process.env.DATABASE_URL;
 
 module.exports = {
   type: 'postgres',
@@ -11,6 +12,7 @@ module.exports = {
   username: USER_NAME,
   password: PASSWORD,
   database: DATABASE,
+  url: URL,
   synchronize: true,
   logging: true,
   entities: [
